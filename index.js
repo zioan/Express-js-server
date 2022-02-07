af805aaa8f92cc4ae0bcc89249f5db0997dbf1b2
Zaharia Ioan <67908472+zioan@users.noreply.github.com>
2022-02-07 12:15:34 +0100

    enable CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -8,6 +9,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "httl://localhost:3000",
+  })
+);
 
 app.listen(5000, () => console.log("Server listening on port 5000"));
 
