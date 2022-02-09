af847f371adcfd43fb72ed99fd96290b599c3948
Zaharia Ioan <67908472+zioan@users.noreply.github.com>
2022-02-09 10:54:40 +0100

    add netlify CORS origin

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://snippet-manager-test.netlify.app",
+    ],
     credentials: true,
   })
 );
